e1e8ff1c829c1aca8cf51d0ff3804721223f3e21
Jochen <info@testingbot.com>
2015-11-13 10:09:03 +0100

    travis build

---
+++ test_travis.py
@@ -0,0 +1,15 @@
+import os
+import unittest
+
+import testingbotclient
+
+class TestTestingBotClient(unittest.TestCase):
+
+    def setUp(self):
+        self.tb = testingbotclient.TestingBotClient()
+
+    def test_get_user_information(self):
+        self.assertEqual(self.tb.user.get_user_information()['first_name'], "travisbot")
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
